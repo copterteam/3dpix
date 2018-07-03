bca6947e0eb4782e6da566bb910376784c02cbea
copterteam <rucopterteam@gmail.com>
2018-07-03 15:44:26 +0300

    header-footer

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
 <div class="header-content">
 <a href="/">
-<figure class="logo"></figure><span class="logoname">KB<br>GO</span><span class="logodesc"><strong>К</strong>онструкторское бюро<br><strong>Г</strong>орбунова А.А.</span>
+<figure class="logo"></figure><span class="logoname">-</span><span class="logodesc"></span>
 </a>
 <div class="phonebox">
  (812) 309-74-15
@@ -17,10 +17,9 @@
 <input type="text" name="userphone" placeholder="Телефон">
 <button>Жду звонка</button>
 
-<span class="agreement">Нажимая на кнопку, вы даете <a href="http://www.kbgo.ru/agreement" target="_blank">согласие на обработку</a> своих персональных данных</span>
+<span class="agreement">Нажимая на кнопку, вы даете <a href="http://www.3dpix.ru/agreement" target="_blank">согласие на обработку</a> своих персональных данных</span>
 
 
-<div id="xc"><div class="x1"></div><div class="x2"></div></div>
 </form>
 
 </div>
